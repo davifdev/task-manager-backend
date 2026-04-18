6a2dab2eedc05a7cd2d1782f237b95cd43f464a0
Davi Fernandes <davi.fer159@gmail.com>
2026-04-18 11:58:49 -0300

    feat: create auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import { type NextFunction, type Request, type Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const accessToken = req.headers.authorization?.split(' ')[1];
+
+    if (!accessToken) {
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    const tokenPayload = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET_KEY as string,
+    ) as { userId: string };
+
+    req.userId = tokenPayload.userId;
+
+    next();
+  } catch (error) {
+    res.status(500).json({
+      message: error instanceof Error ? error.message : 'Internal Server Error',
+    });
+  }
+};
