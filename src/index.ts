2b5fee5833e036b00135771f475c0366335fe506
Davi Fernandes <davi.fer159@gmail.com>
2026-05-18 15:53:40 -0300

    feat: add express and create a test server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import "dotenv/config";
+import express from "express";
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
