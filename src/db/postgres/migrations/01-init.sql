ce92e772ad075a74d6602f07ac35bc4988b36505
Davi Fernandes <davi.fer159@gmail.com>
2026-05-20 19:26:53 -0300

    feat: add migrations

---
+++ src/db/postgres/migrations/01-init.sql
@@ -0,0 +1,51 @@
+CREATE TABLE users (
+  id UUID PRIMARY KEY NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TYPE task_status AS ENUM (
+  'is_pending',
+  'in_progress', 
+  'is_completed'
+);
+
+CREATE TYPE task_time AS ENUM (
+  'morning', 
+  'afternoon', 
+  'evening'
+);
+
+CREATE TABLE tasks (
+  id UUID PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  status task_status NOT NULL,
+  time task_time NOT NULL,
+
+  user_id UUID NOT NULL,
+
+  CONSTRAINT fk_tasks_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE refresh_tokens (
+  id UUID PRIMARY KEY NOT NULL,
+  token_hash VARCHAR(255) NOT NULL,
+  family_id VARCHAR(255) NOT NULL,
+
+  user_id UUID NOT NULL,
+
+  revoked BOOLEAN NOT NULL DEFAULT FALSE,
+  expires_at TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_refresh_tokens_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
---
+++ src/db/postgres/migrations/exec.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import { pool } from "../client";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const execMigrations = async () => {
+  const client = await pool.connect();
+
+  try {
+    const files = fs
+      .readdirSync(__dirname)
+      .filter(file => file.endsWith(".sql"));
+
+    for (const file of files) {
+      const filePath = path.join(__dirname, file);
+      const script = fs.readFileSync(filePath, "utf-8");
+      await client.query(script);
+
+      console.log(`Migration for file ${file} executed successfully.`);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.release();
+  }
+};
+
+execMigrations();
