fc438f9afe9ac628f8343a015e8ed4c114e93b24
Davi Fernandes <davi.fer159@gmail.com>
2026-05-21 18:23:52 -0300

    feat: add CreateTaskController

---
+++ src/controllers/tasks/create-task.ts
@@ -0,0 +1,30 @@
+import { type Request } from "express";
+import type { CreateTaskUseCase } from "../../use-cases/tasks/create-task";
+import type { BodyParams } from "../../models/tasks/create-task";
+
+export class CreateTaskController {
+  private readonly createTaskUseCase;
+
+  constructor(createTaskUseCase: CreateTaskUseCase) {
+    this.createTaskUseCase = createTaskUseCase;
+  }
+
+  async execute(httpRequest: Request) {
+    try {
+      const params: BodyParams = httpRequest.body;
+
+      const result = await this.createTaskUseCase.execute(params);
+
+      return {
+        statusCode: 201,
+        body: result,
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        statusCode: 500,
+        body: "Internal Server Error",
+      };
+    }
+  }
+}
