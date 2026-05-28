9394419875d59b3b58d41c0edfc3d948b9f68804
Davi Fernandes <davi.fer159@gmail.com>
2026-05-28 17:50:37 -0300

    fix: ensure GetTasksRepository returns array of tasks

--- src/repositories/tasks/get-task.ts
+++ src/repositories/tasks/get-task.ts
@@ -2,7 +2,7 @@ import { PostgresClient } from "../../db/postgres/client";
 
 export class GetTasksRepository {
   async execute() {
-    const result = await PostgresClient.query("SELECT * FROM tasks ");
-    return result[0];
+    const result = await PostgresClient.query("SELECT * FROM tasks");
+    return result;
   }
 }
