486655f0d56f9431e34408c34a6882cccc3c1380
Davi Fernandes <davi.fer159@gmail.com>
2026-05-29 20:46:03 -0300

    refactor: remove incorrect validation user_id

--- src/controllers/tasks/create-task.ts
+++ src/controllers/tasks/create-task.ts
@@ -1,9 +1,7 @@
 import { type Request } from "express";
 import type { CreateTaskUseCase } from "../../use-cases/tasks/create-task";
 import type { BodyParams } from "../../models/tasks/create-task";
-import validator from "validator";
 import { badRequest, create, serverError } from "../../helpers/http";
-import { idIsInvalidResponse } from "../../helpers/message-response";
 
 type RequiredFields = keyof BodyParams;
 export class CreateTaskController {
@@ -16,12 +14,6 @@ export class CreateTaskController {
   async execute(httpRequest: Request) {
     try {
       const params: BodyParams = httpRequest.body;
-      const userId = params.user_id;
-      const checkIfIsValidId = validator.isUUID(userId);
-
-      if (!checkIfIsValidId) {
-        return idIsInvalidResponse();
-      }
 
       const requiredFields: RequiredFields[] = [
         "title",
