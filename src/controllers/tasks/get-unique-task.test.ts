4908a6c8acf31fb39054ad2d6577d4b1c1ff4158
Davi Fernandes <davi.fer159@gmail.com>
2026-06-12 20:24:01 -0300

    test: should return 200 if unique taks return with successfully

---
+++ src/controllers/tasks/get-unique-task.test.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { faker } from "@faker-js/faker";
+import { task } from "../../__tests__/tasks/create-task";
+import { GetUniqueTaskController } from "./get-unique-task";
+
+describe("GetUniqueTaskController", () => {
+  class GetUniqueTaskUseCase {
+    async execute() {
+      return task;
+    }
+  }
+
+  const makeSut = () => {
+    const getUniqueTaskUseCase = new GetUniqueTaskUseCase();
+    const sut = new GetUniqueTaskController(getUniqueTaskUseCase as any);
+
+    return {
+      sut,
+      getUniqueTaskUseCase,
+    };
+  };
+
+  const httpRequest = {
+    params: {
+      taskId: faker.string.uuid(),
+    },
+  };
+
+  it("should return 200 if task returned with successfully", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute(httpRequest as any);
+
+    expect(response.statusCode).toBe(200);
+  });
+});
