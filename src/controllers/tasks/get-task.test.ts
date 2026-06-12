0f7e39a590d1d1601b5d5122134f9328dea0c20c
Davi Fernandes <davi.fer159@gmail.com>
2026-06-12 20:05:19 -0300

    test: should throw TaskNotFound throws

--- src/controllers/tasks/get-task.test.ts
+++ src/controllers/tasks/get-task.test.ts
@@ -2,6 +2,7 @@
 import { faker } from "@faker-js/faker";
 import { task } from "../../__tests__/tasks/create-task";
 import { GetTasksController } from "./get-task";
+import { TaskNotFound } from "../../helpers/errors";
 
 describe("GetTasksController", () => {
   class GetTasksUseCaseStub {
@@ -54,5 +55,17 @@ describe("GetTasksController", () => {
     const response = await sut.execute(httpRequest as any);
 
     expect(response.statusCode).toBe(500);
+  });
+
+  it("should throw TaskNotFound throws", async () => {
+    const { sut, getTasksUseCase } = makeSut();
+
+    vi.spyOn(getTasksUseCase, "execute").mockImplementation(() => {
+      throw new TaskNotFound();
+    });
+
+    const response = await sut.execute(httpRequest as any);
+
+    expect(response.statusCode).toBe(400);
   });
 });
