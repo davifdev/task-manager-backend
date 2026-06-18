940b7e13b4dd08ca402080dd8d4476ed701de19f
Davi Fernandes <davi.fer159@gmail.com>
2026-06-18 20:56:19 -0300

    docs: add docs with swagger

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,24 @@ import express from "express";
 
 import { userRouter } from "./routes/users";
 import { tasksRouter } from "./routes/tasks";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRouter);
 app.use("/api/tasks", tasksRouter);
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(join(__dirname, "../docs/swagger.json"), "utf-8"),
+);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
