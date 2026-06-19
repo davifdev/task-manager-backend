50ea02efd6cb5eb934a4b0cd4182055875b8b3dc
Davi Fernandes <davi.fer159@gmail.com>
2026-06-19 13:43:55 -0300

    chore: add connectionString in postgres with pg

--- src/db/postgres/client.ts
+++ src/db/postgres/client.ts
@@ -1,23 +1,21 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-console.log("minha porta", process.env.POSTGRES_PORT);
 
 export const PostgresClient = {
   query: async <T>(query: string, params?: T[]) => {
     const client = await pool.connect();
 
-    const result = await client.query(query, params);
-
-    client.release();
-
-    return result.rows;
+    try {
+      const result = await client.query(query, params);
+      return result.rows;
+    } finally {
+      client.release();
+    }
   },
 };
