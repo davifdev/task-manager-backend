74ee42b50b76cb6789d0170bf0e1495b5268e887
Davi Fernandes <davi.fer159@gmail.com>
2026-06-19 11:13:17 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,14 @@ import swaggerUi from "swagger-ui-express";
 import fs from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/tasks", tasksRouter);
 
